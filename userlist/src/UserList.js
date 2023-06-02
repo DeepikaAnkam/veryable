22cce0f47727d1375c6d43b6d7b602935af03915
Deepika Ankam <deepika.ankam07@gmail.com>
2023-06-02 19:28:22 -0400

    imports

--- userlist/src/UserList.js
+++ userlist/src/UserList.js
@@ -1,3 +1,9 @@
+import React, {useState} from 'react';
+import usersData from './users.json';
+import { ReactComponent as UserIcon } from './icons/user.svg';
+import { ReactComponent as ExpandMoreIcon } from './icons/expand_more.svg';
+import { ReactComponent as ExpandLessIcon } from './icons/expand_less.svg';
+
 const UserList = () => {
     return;
 }
