d3e8f210c27ddf7829edc42f3a293b580d4b98c9
Deepika Ankam <deepika.ankam07@gmail.com>
2023-06-02 21:15:38 -0400

    without expansion styling

--- userlist/src/UserList.js
+++ userlist/src/UserList.js
@@ -44,6 +44,45 @@ const UserCard = ({user, expanded, onCardClick}) => {
                 return '#7E7E7E';
         }
     }
-}
+
+
+
+return (
+    <>
+        <div 
+            style={{
+                backgroundColor: '#FFFFFF',
+                margin: expanded ? '16px 12px 0 12px' : '16px 12px 16px 12px',
+                height: '80px',
+                display: 'flex',
+                alignItems: 'center'
+            }}
+            onClick={handleClick}
+        > 
+            <div style={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                marginLeft: '12px'
+            }} >
+                <UserIcon fill={getIconColor()}/>
+            </div> 
+            <div style={{marginLeft: '12px'}}>
+                <div style={{fontSize: '12px', fontWeight: '600', color: '#4A4A4A'}}>{`${firstName} ${lastName}`}</div>
+                <div style={{fontSize: '10px',  color: '#4A4A4A'}}>{role}</div>
+                <div style={{fontSize: '10px',  color: '#7E7E7E'}}>{email}</div>
+            </div> 
+            <div style={{float: "right", marginLeft: "auto", marginRight: "12px"}}>
+          {expanded ? <ExpandLessIcon/> :
+            <ExpandMoreIcon/>
+          }
+        </div>
+      </div>
+
+
+  
+    </>
+    );
+  };
 
 export default UserList;
