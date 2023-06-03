c9bc5fd13f5792f5326c8531ad0a06a6380c5c66
Deepika Ankam <deepika.ankam07@gmail.com>
2023-06-02 20:53:46 -0400

    added functionality

--- userlist/src/UserList.js
+++ userlist/src/UserList.js
@@ -5,7 +5,45 @@ import { ReactComponent as ExpandMoreIcon } from './icons/expand_more.svg';
 import { ReactComponent as ExpandLessIcon } from './icons/expand_less.svg';
 
 const UserList = () => {
-    return;
+    const [expandedUserId, setExpandedUserId] = useState(null);
+
+    const handleCardClick = (userId) => {
+        setExpandedUserId(userId === expandedUserId ? null : userId);
+    }
+
+    return (
+        <div>
+            {usersData.map((user) => (
+                <UserCard
+                    key={user.id}
+                    user={user}
+                    expanded={user.id === expandedUserId}
+                    onCardClick={handleCardClick}
+                />
+            ))}
+        </div>
+    );
+};
+
+const UserCard = ({user, expanded, onCardClick}) => {
+    const {id, firstName, lastName, role, email, street, city, state, zip, phone, createdAt, lastLoggedIn} = user;
+
+    const handleClick = () => {
+        onCardClick(id);
+    };
+
+    const getIconColor = () => {
+        switch (role) {
+            case 'Administrator':
+                return '#2081C3';
+            case 'User':
+                return '#68AAAB';
+            case 'Viewer':
+                return '#7E7E7E';
+            default:
+                return '#7E7E7E';
+        }
+    }
 }
 
 export default UserList;
